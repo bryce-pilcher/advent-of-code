0564d35163f9eec51323911481103323b25887ff
Bryce Pilcher <bryce.pilcher@elderresearch.com>
2022-12-08 07:27:54 -0500

    day 7

--- src/main/python/advent_of_code/2022/day7.py
+++ src/main/python/advent_of_code/2022/day7.py
@@ -1,25 +1,72 @@
+from __future__ import annotations
+
+import os.path
+from typing import List
 from utils import get_file
 
 
+class Directory:
+    name: str
+    parent: Directory
+    size: int
+    children: List
+
+    def __init__(self, name, parent = None, size = 0):
+        self.name = name
+        self.parent = parent
+        self.size = size
+        self.children = []
+
+
 if __name__ == '__main__':
-    with get_file('day6.txt') as input:
+    with get_file('day7.txt') as input:
         lines = input.readlines()
+        directories = {}
+        cur_dir = Directory('/')
+        directories[cur_dir.name] = cur_dir
 
         for line in lines:
-            position = 1
-            last_four_characters = []
-            for char in line:
-                if len(last_four_characters) == 14:
-                    last_four_characters.pop(0)
-                last_four_characters.append(char)
+            cleaned_line = line.strip()
+            info = cleaned_line.split(' ')
+            if info[0] == 'dir':
+                name = os.path.join(cur_dir.name, info[1])
+                if name not in directories:
+                    dir = Directory(name, cur_dir)
+                    directories[dir.name] = dir
+            elif info[1] == 'cd':
+                if info[2] == '..':
+                    cur_dir = cur_dir.parent
+                else:
+                    name = os.path.join(cur_dir.name, info[2])
+                    cur_dir = directories[name]
+            elif info[1] == 'ls':
+                continue
+            else:
+                size = int(info[0])
+                cur_dir.size += size
+                cur_dir.children.append(info[1])
+                par_dir = cur_dir.parent
+                while par_dir:
+                    if par_dir:
+                        par_dir.size += size
+                        par_dir = par_dir.parent
 
-                if len(set(last_four_characters)) == 14:
-                    break
+        total_size = 0
+        for v in directories.values():
+            if v.size <= 100000:
+                total_size += v.size
 
-                position += 1
+        print(total_size)
 
-            print(position)
+        remaining = 70000000 - directories['/'].size
+        size_needed = 30000000 - remaining
 
+        val_list = [v.size for v in directories.values()]
+        val_list.sort()
+        for v in val_list:
+            if v > size_needed:
+                print(v)
+                break
 
 
 
